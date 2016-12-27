2447774e5aa5b03cc7ff84de44c9c3cd6e30cdb7
denniseh7 <denniseh7@gmail.com>
2016-12-26 16:25:17 -0800

    11th drug tweet start.

--- LivestreamTest.py
+++ LivestreamTest.py
@@ -38,7 +38,7 @@ class MyStreamListener(tweepy.StreamListener):
                     if "http" not in tweet:
                         output=' '.join(tweet.split())
                         output+='\n'
-                        f = open('drugtweet10.txt', 'ab')
+                        f = open('drugtweet11.txt', 'ab')
                         f.write(output.encode('utf-8'))
                         f.close()
 
